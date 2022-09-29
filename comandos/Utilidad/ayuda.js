f10e51a54761d60e279bef61055a61ce4c2c5b7a
Un usuario random <73328182+ElYisus100@users.noreply.github.com>
2022-09-28 19:45:20 -0500

    Update ayuda.js

--- comandos/Utilidad/ayuda.js
+++ comandos/Utilidad/ayuda.js
@@ -1,3 +1,5 @@
+// copia la Estructura de comandos cada vez que quieras hacer un nuevo comando
+
 const Discord = require("discord.js")
 module.exports = {  
 name: "ayuda", // importante ponerle nombre al comando
