cd15954ea21baa55422bdf57cef1217b60cc5378
Un usuario random <73328182+ElYisus100@users.noreply.github.com>
2022-09-28 19:39:50 -0500

    Create messageCreate.js

---
+++ eventos/messageCreate.js
@@ -0,0 +1,16 @@
+const prefix = "/";
+
+module.exports = async (client, message) => {
+  
+if(message.author.bot)return;
+if(message.channel.type == "DM")return;
+
+  
+if(!message.content.startsWith(prefix))return;
+const args = message.content.slice(prefix.length).trim().split(/ +/g); 
+const command = args.shift().toLowerCase()
+
+let cmd = client.commands.find((c) => c.name === command || c.alias && c.alias.includes(command))
+if(cmd)cmd.run(client, message, args)
+  
+}
